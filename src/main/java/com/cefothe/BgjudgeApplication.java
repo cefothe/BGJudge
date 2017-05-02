2d2307eb59d45f5e241e6621dd6dbd5bc66ef01b
Stefan Angelov <cefothe@gmail.com>
2017-05-02 20:12:52 +0300

    Initialize spring boot application

---
+++ src/main/java/com/cefothe/BgjudgeApplication.java
@@ -0,0 +1,12 @@
+package com.cefothe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgjudgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgjudgeApplication.class, args);
+	}
+}
