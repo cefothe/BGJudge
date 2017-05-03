c0375d4a173d49498741d52ca09b8e681f22c0b4
Stefan Angelov <cefothe@gmail.com>
2017-05-04 00:55:39 +0300

    home page

--- src/main/java/com/cefothe/configuration/SecurityConfiguration.java
+++ src/main/java/com/cefothe/configuration/SecurityConfiguration.java
@@ -38,7 +38,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/register", "/bootstrap/**", "/jquery/**").permitAll()
+                    .antMatchers("/", "/register", "/css/**", "/js/**").permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .formLogin().loginPage("/login").permitAll()
