4ca2f4324273e20f44dcf900317b4340b994722d
Stefan Angelov <cefothe@gmail.com>
2017-05-04 01:06:55 +0300

    Add message for log start on application

--- src/main/java/com/cefothe/BGJudgeApplication.java
+++ src/main/java/com/cefothe/BGJudgeApplication.java
@@ -1,12 +1,29 @@
 package com.cefothe;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class BGJudgeApplication {
 
+	@Value("${environment.name}")
+	private String environmentName;
+
+	private static final Logger LOG = LoggerFactory.getLogger(BGJudgeApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(BGJudgeApplication.class, args);
 	}
+
+	@PostConstruct
+	public void postConstruct(){
+		LOG.info("BG Judge initialized. Environment = {}", environmentName);
+	}
+
 }
