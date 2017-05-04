09772655d37fa6ba235c7ed9321e014380edc0f3
Stefan Angelov <cefothe@gmail.com>
2017-05-04 20:42:44 +0300

    save worl

--- src/main/java/com/cefothe/exam/services/ExamService.java
+++ src/main/java/com/cefothe/exam/services/ExamService.java
@@ -1,12 +1,13 @@
 package com.cefothe.exam.services;
 
 import com.cefothe.exam.entitities.Examens;
+import com.cefothe.user.entities.User;
 
 /**
  * Created by cefothe on 04.05.17.
  */
 public interface ExamService {
-    void create(Examens examens);
+    void create(Examens examens, User user);
     Examens update(Examens examens);
     void delete(Long id);
 }
--- src/main/java/com/cefothe/exam/services/ExamServiceBean.java
+++ src/main/java/com/cefothe/exam/services/ExamServiceBean.java
@@ -1,6 +1,9 @@
 package com.cefothe.exam.services;
 
 import com.cefothe.exam.entitities.Examens;
+import com.cefothe.exam.repositories.ExamRepository;
+import com.cefothe.user.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -9,11 +12,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class ExamServiceBean implements ExamService {
 
+    private ExamRepository examRepository;
 
+    @Autowired
+    public ExamServiceBean(ExamRepository examRepository) {
+        this.examRepository = examRepository;
+    }
 
     @Override
-    public void create(Examens examens) {
-
+    public void create(Examens examens, User user) {
+        examRepository.save(examens);
     }
 
     @Override
