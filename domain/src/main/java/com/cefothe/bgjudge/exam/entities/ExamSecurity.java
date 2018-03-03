2846120568f92cd69de8b282d39ac1d05903dcf5
Stefan Angelov <cefothe@gmail.com>
2018-03-03 09:46:20 +0200

    create a method that validate password and create a test for it

--- domain/src/main/java/com/cefothe/bgjudge/exam/entities/ExamSecurity.java
+++ domain/src/main/java/com/cefothe/bgjudge/exam/entities/ExamSecurity.java
@@ -5,7 +5,10 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,4 +26,13 @@ public class ExamSecurity extends BaseEntity {
 
     @ElementCollection
     private List<String> allowedIps = new ArrayList<>();
+
+    /**
+     * This method validate if the password that user is apply is the same as {@link Examens} password
+     * @param password the password from user
+     * @return if the password is correct
+     */
+    public boolean checkForCorrectPassword(String password){
+        return this.password.equals(password);
+    }
 }
